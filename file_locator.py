be4db308dd4fa724c89272f26b68ade2d4b1f40e
Gopaal <gopalknowsyou@gmail.com>
2014-03-21 16:49:44 +0530

    updated file locator

--- file_locator.py
+++ file_locator.py
@@ -1,16 +1,27 @@
 import os
 from fnmatch import fnmatch
+import mimetypes
 
-root = '/home/labadmin'
+root = '/home/labadmin/.sugar/default/datastore/43'
 pattern = "*.txt"
 a = []
+b = []
 
 for path, subdirs, files in os.walk(root):
     for name in files:
+	if mimetypes.guess_type(name) == (None, None):
+		a.append(path)
+		b.append(name)
+		print path, name
+	"""
         if fnmatch(name, pattern):
             print os.path.join(path, name)
-	    a.append(path)
+	    a.append(name)
+	"""
 	    
+print "\n \n"
+print a[3], b[3]
 	    
-for v in a:
-	print " value: ", v
+#for v in a:
+#	print " value: ", v
+
